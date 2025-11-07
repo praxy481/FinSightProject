5cf5c7e0a7f82b457c52a5105db315f5e76da1cd
Shriya Sankhe <shriyasankhe25@gmail.com>
2025-11-08 01:28:43 +0530

    Ready to deploy

--- middleware.js
+++ middleware.js
@@ -41,13 +41,13 @@ const clerk = clerkMiddleware(async (auth, req) => {
 });
 
 
-export default createMiddleware(aj, clerk);
-
 export const config = {
+  runtime: "nodejs",
   matcher: [
     
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    
+    "/",
     "/(api|trpc)(.*)",
   ],
 };
+ 
